1b897ddffab054f89445cd4bf68e4f0960659426
sandhya-sake <sandhyasake93@gmail.com>
2023-10-31 18:17:44 +0530

    first commit

---
+++ src/test/java/stepdefinition/Mainsteps.java
@@ -0,0 +1,28 @@
+package stepdefinition;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Mainsteps {
+	
+	@Given("I am on Ebay home page")
+	public void i_am_on_ebay_home_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	   
+		System.out.println("User landed on ebay home page");
+	}
+	@When("I click on advanced link")
+	public void i_click_on_advanced_link() {
+	    // Write code here that turns the phrase above into concrete actions
+	   // throw new io.cucumber.java.PendingException();
+		System.out.println("User click on advanced link");
+	}
+	@Then("I navigate to advanced search page")
+	public void i_navigate_to_advanced_search_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	   // throw new io.cucumber.java.PendingException();
+		System.out.println("User navigate to advanced search page");
+	}
+
+}
---
+++ target/test-classes/features/firstpgm.feature
@@ -0,0 +1,10 @@
+
+@tag
+Feature: Ebay Home page scenario
+  @tag1
+  Scenario: advanced search link
+    Given I am on Ebay home page
+    When I click on advanced link
+    Then I navigate to advanced search page
+
+  
